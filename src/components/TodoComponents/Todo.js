b157ae28344dbf49213bcdaa8bc0f1238d8395ea
ericbanker12 <ericbanker12@gmail.com>
2019-09-30 16:32:02 -0500

    make search case insensitive, stage Search.js... oops...

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -8,7 +8,7 @@ class Todo extends React.Component {
             <li
                 className={this.props.completed?'completed text-danger':''}
                 onClick={()=>{this.props.handler(this.props.id)}}
-                hidden={this.props.task.includes(this.props.search)?false:true}
+                hidden={RegExp(this.props.search,'i').test(this.props.task)?false:true}
             >
                 {this.props.task}
             </li>
